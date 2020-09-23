31074f2f1d65cff1073a1d73e256334dde3ec763
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-23 16:42:10 +0900

    タスク作成機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,22 @@
 class TasksController < ApplicationController
   def index
+    # タスク一覧表示させる
+    @tasks = Task.all
   end
 
   def new
-    # form_withに渡す引数（空のインスタンス）
+    # form_withに渡す引数（空のインスタンスをform_withで利用する）
     @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :deadline, :category_id, :priority_id)
   end
 end
