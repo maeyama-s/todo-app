4f7b9b6ffc681bad9aec747f8dd240cd0a963fda
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-23 16:44:31 +0900

    コメント

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,8 +14,9 @@ class TasksController < ApplicationController
     redirect_to root_path
   end
 
+  # Classの外部から呼ばれたら困るメソッドを隔離、可読性向上
   private
-
+  # 指定したキーを持つパラメーターのみを受け取るように制限する
   def task_params
     params.require(:task).permit(:title, :details, :deadline, :category_id, :priority_id)
   end
