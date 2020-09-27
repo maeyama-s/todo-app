91273c582eec893e298878f9270342439a3d6ddd
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-27 15:07:28 +0900

    1日後に同じタスクを複製するボタン追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def repeat
+    task = Task.find(params[:id])
+    task = Task.new(
+                    user_id: task.user_id, title: task.title, details: task.details, deadline: task.deadline + 1,
+                    category_id: task.category_id, priority_id: task.priority_id
+                  )
+    task.save
+  end
+
   # Classの外部から呼ばれたら困るメソッドを隔離、可読性向上
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tasks#top"
   resources :tasks
+  post 'tasks/:id/repeat', to: 'tasks#repeat'
 end
