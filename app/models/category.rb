652ec2f4d3feeeea392dd055af20591ea5552ee8
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-10-12 15:52:45 +0900

    newコーディング

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,8 @@
 # ActiveHash::Baseは、ActiveRecordと同様のメソッドが使用できるようになる。
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'プライベート' },
-    { id: 3, name: '仕事' },
-    { id: 4, name: 'その他' }
+    { id: 1, name: 'プライベート' },
+    { id: 2, name: '仕事' },
+    { id: 3, name: 'その他' }
   ]
 end
--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,10 +1,9 @@
 # ActiveHash::Baseは、ActiveRecordと同様のメソッドが使用できるようになる。
 class Priority < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '重要度が高く、緊急度も高い' },
-    { id: 3, name: '重要度は低く、緊急度が高い' },
-    { id: 4, name: '重要度が高く、緊急度は低い' },
-    { id: 5, name: '重要度は低く、緊急度も低い' }
+    { id: 1, name: '重要度が高く、緊急度も高い' },
+    { id: 2, name: '重要度は低く、緊急度が高い' },
+    { id: 3, name: '重要度が高く、緊急度は低い' },
+    { id: 4, name: '重要度は低く、緊急度も低い' }
   ]
 end
