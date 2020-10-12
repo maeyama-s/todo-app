20120306063b7281e320d210d8d855e96eb1ede8
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-10-12 18:19:19 +0900

    deviseのレイアウト崩れ対応、タイムゾーン設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
   # 他テーブルとのアソシエーション
   has_many :tasks
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,13 @@ module TodoApp
     # 日本語の言語設定
     config.i18n.default_locale = :ja
 
+    # devise画面にてエラーが出るとラベルとインプットエリアの表示がおかしくなる現象の対応
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    
+    # タイムゾーン設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
