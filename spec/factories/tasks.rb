eecbd7b199672d4c39f881607184041543835ed3
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-10-21 21:32:59 +0900

    コントローラーテストでFactoryBot.createで記号でエラー出る

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :task do
     user_id     { Faker::Number.between(from: 1, to: 100) }
-    title       { Faker::String.random(length: 5..10) }
-    details     { Faker::String.random(length: 5..10) }
+    title       { Faker::Name.initials(number: 5) }
+    details     { Faker::Name.initials(number: 10) }
     deadline    { Faker::Date.between(from: '2015-01-01', to: '2025-12-31') }
     category_id { Faker::Number.between(from: 1, to: 3) }
     priority_id { Faker::Number.between(from: 1, to: 4) }
