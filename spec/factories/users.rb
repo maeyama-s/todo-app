e16f4b7dda38e05a92767fdfd0f77598dc2a3662
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-10-21 18:52:57 +0900

    password生成異常の対応

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
-    email                 {"abc@email"} # 一意性チェックで引っかかることがあるためFaker使っていない
-    password              {Faker::Internet.password(min_length: 6)} # Fakerで英数字混合の指定できないため、たまに引っかかる
+    email                 {Faker::Internet.free_email}
+    password              {"abc123"}
     password_confirmation {password}
   end
 end
