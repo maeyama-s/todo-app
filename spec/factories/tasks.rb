b27e8851e93daa64e7a07ad0fc81912caaf61328
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-10-21 21:09:09 +0900

    Task FactoryBot修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     title       { Faker::String.random(length: 5..10) }
     details     { Faker::String.random(length: 5..10) }
     deadline    { Faker::Date.between(from: '2015-01-01', to: '2025-12-31') }
-    category_id { Faker::Number.between(from: 1, to: 2) }
+    category_id { Faker::Number.between(from: 1, to: 3) }
     priority_id { Faker::Number.between(from: 1, to: 4) }
     association :user # Taskのインスタンスが生成したと同時に、関連するUserのインスタンスも生成される。Taskに対しては、必ずUserが紐付いている必要があるため、このように記述する必要がある。
   end
