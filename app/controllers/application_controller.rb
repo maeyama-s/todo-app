bed4ae48f6d7e1649cce9d90b0b87c7358061f43
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2021-07-20 21:31:00 +0900

    Basic認証削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # deviseに関する処理であるときのみ、configure_permitted_parametersメソッドを実行するように設定
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  
+  # 2021.7/20 Basic認証をなくすためにcomment out
+  # before_action :basic_auth
 
   private
 
@@ -10,11 +12,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
+  # 2021.7/20 Basic認証をなくすためにcomment out
+  # Basic認証
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+  #   end
+  # end
 
   # deviseでのログイン後に遷移するパス設定
   def after_sign_in_path_for(_resource)
